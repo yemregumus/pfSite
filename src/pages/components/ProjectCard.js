5cd22a500617cd5765606b367d1f44dccbb80a22
yemregumus <ygumus@myseneca.ca>
2024-05-03 11:48:16 -0400

    Portfolio page has been designed, cards added and style.

--- src/pages/components/ProjectCard.js
+++ src/pages/components/ProjectCard.js
@@ -6,22 +6,21 @@ const ProjectCard = ({ name, description, githubRepo, deploymentLink, imagePath 
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <motion.div className="project-card" whileHover={{ scale: 1.05, height: "auto" }} initial={{ scale: 1, height: "18rem" }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-      <Image src={imagePath} alt={name} width={300} height={200} />
-      <div>
-        <h3 className="text-xl font-bold mb-4">{name}</h3>
-        <p className="text-gray-600 mb-6" style={{ display: isHovered ? "block" : "none" }}>
-          {description}
-        </p>
-      </div>
-      <div className="flex space-x-4">
-        <a href={githubRepo} className="text-green-500 font-semibold hover:text-green-600">
-          GitHub Repo
-        </a>
-        <a href={deploymentLink} className="text-green-500 font-semibold hover:text-green-600">
-          Deployment Link
-        </a>
-      </div>
+    <motion.div className="project-card-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <motion.div className="project-card" whileHover={{ scale: 1.05, height: "auto" }} initial={{ scale: 1, height: "18rem" }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <Image src={imagePath} alt={name} width={300} height={200} />
+        <div>
+          <h3 className="card-title text-xl font-bold mb-4">{name}</h3>
+          <p className="text-gray-600 mb-6" style={{ display: isHovered ? "block" : "none" }}>
+            {description}
+          </p>
+        </div>
+        <div className="flex space-x-4">
+          <a href={githubRepo} className="text-green-500 font-semibold hover:text-green-600">
+            Link To GitHub Repo
+          </a>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,69 +2,68 @@ import React from "react";
 import { motion } from "framer-motion";
 import ProjectCard from "./components/ProjectCard";
 import PortfolioSS from "../img/portfolio.jpg";
+import PythonBoardGameSS from "../img/pygame.jpg";
+import MovieLibrarySS from "../img/movielib.jpg";
+import MuseumOfArtSS from "../img/mmoany.jpg";
+import MuseumOfArtBackendSS from "../img/mmoanybackend.jpg";
+import PfBackendSS from "../img/pfbackend.jpg";
+import FragmentsUISs from "../img/fragmentsUI.jpg";
+import FragmentsBackendSS from "../img/fragmentsMS.jpg";
 
 const Portfolio = () => {
   const projects = [
     {
       name: "My Portfolio Website",
-      description: "React.JS, Next.JS, TailwindCSS, Framer-Motion",
+      description: "Built with React.JS, Next.JS, TailwindCSS, Framer-Motion",
       githubRepo: "https://github.com/yemregumus/pfSite",
-      imageSrc: PortfolioSS,
-      deploymentLink: "",
+      imagePath: PortfolioSS,
     },
     {
       name: "Python Board Game",
       description: "DSA Project, this is a two player board game. Implemented with Python.",
       githubRepo: "https://github.com/yemregumus/pythonBoardGame",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: PythonBoardGameSS,
     },
     {
       name: "Movie Library Website",
-      description: "React and Next.js, interface for viewing movie details.",
+      description: "Built with React and Next.js, interface for viewing movie details.",
       githubRepo: "https://github.com/yemregumus/WEB422-Assignment3",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: MovieLibrarySS,
     },
     {
       name: "Museum of Art New York Website",
-      description: "React, Next.js, and React Bootstrap. responsive user interface for browsing art collections.",
+      description: "Built with React, Next.js, and React Bootstrap. responsive user interface for browsing art collections.",
       githubRepo: "https://github.com/yemregumus/WEB422-Assignment6",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: MuseumOfArtSS,
     },
     {
       name: "Museum of Art New York Website Backend",
       description: "This backend server, built with Express.js, facilitates user authentication and data management for a web application.",
       githubRepo: "https://github.com/yemregumus/a6Backend",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: MuseumOfArtBackendSS,
     },
     {
       name: "Backend for My Portfolio Website's Contact Page",
       description: "This backend server, built with Node.js and Express.js, facilitates communication between clients and a designated email recipient. ",
       githubRepo: "https://github.com/yemregumus/pfContactAPI",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: PfBackendSS,
     },
     {
       name: "Fragments MicroService UI (Frontend)",
       description: "This UI provides an intuitive interface for creating, editing, and deleting fragments.",
       githubRepo: "https://github.com/yemregumus/fragments-ui",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: FragmentsUISs,
     },
     {
       name: "Fragments MicroService UI (Backend)",
       description: "The fragments microservice facilitates the management of text, image, and JSON data fragments within a distributed system.",
       githubRepo: "https://github.com/yemregumus/fragments",
-      imageSrc: "",
-      deploymentLink: "",
+      imagePath: FragmentsBackendSS,
     },
   ];
 
   return (
-    <motion.section initial={{ opacity: 0, filter: "blur(20px)" }} animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 1, delay: 0.5 }} className="bg-transparent bg-opacity-20 backdrop-filter backdrop-blur-lg mt-4" style={{ minHeight: "70vh" }} id="contact">
+    <motion.section initial={{ opacity: 0, filter: "blur(20px)" }} animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 1, delay: 0.5 }} className="bg-transparent bg-opacity-20 backdrop-filter backdrop-blur-lg mt-4 mb-10" style={{ minHeight: "70vh" }} id="contact">
       <div className="container">
         <div className="row">
           <div className="col text-center">
@@ -80,7 +79,7 @@ const Portfolio = () => {
                 description={project.description}
                 githubRepo={project.githubRepo}
                 deploymentLink={project.deploymentLink}
-                imageSrc={project.imageSrc} // Use imageSrc instead of imagePath
+                imagePath={project.imagePath} // Use imageSrc instead of imagePath
               />
             </div>
           ))}
